ef34e77cece4cc26a44eb3d1ff3366c242d65cbb
Rodrigo Mastropietro <rmpietro@gmail.com>
2024-05-27 23:17:05 -0300

    Added tailwindcss to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Welcome to the Frontend! Rodrigo</h1>
+      <h1 className="text-2xl text-red-500 font-bold bg-yellow-300">
+        Welcome to the Frontend! Rodrigo
+      </h1>
     </div>
   );
 }
